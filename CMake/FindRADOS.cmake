1e2828f0363b62935b49f6fac203380f6d9f0290
Jerome Soumagne <jsoumagne@hdfgroup.org>
2019-04-23 17:08:12 -0500

    CMake: fix typo in find RADOS

--- CMake/FindRADOS.cmake
+++ CMake/FindRADOS.cmake
@@ -12,7 +12,7 @@ find_path(RADOS_INCLUDE_DIR rados/librados.h
   PATHS /usr/local/include /usr/include)
 
 find_library(RADOS_LIBRARY NAMES rados
-  HINTS ${PC_DRC_LIBDIR} ${PC_DRC_LIBRARY_DIRS}
+  HINTS ${PC_RADOS_LIBDIR} ${PC_RADOS_LIBRARY_DIRS}
   PATHS /usr/local/lib64 /usr/local/lib /usr/lib64 /usr/lib)
 
 set(RADOS_INCLUDE_DIRS ${RADOS_INCLUDE_DIR})
