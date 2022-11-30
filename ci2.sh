887dfb5171e537274827994e9e44df6eea0c14ae
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-11-30 13:37:57 -0600

    ci: change path for file_backend

--- ci2.sh
+++ ci2.sh
@@ -29,7 +29,7 @@ echo "Running sysctl"
 
 echo "Testing using ior"
 export HDF5_VOL_CONNECTOR=rados
-export HDF5_PLUGIN_PATH=/home/runner/install/bin/
+export HDF5_PLUGIN_PATH=/home/runner/install/lib/
 export MOBJECT_CLUSTER_FILE=/home/runner/mobject.ssg
 
 bake-mkpool -s 50M /tmp/mobject.dat
